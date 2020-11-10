fd7ff8115d9ccd50bb38a7efa4b9b7729f78b9a9
Marfy Cheese <ckenney@thegraysonschool.org>
2020-11-10 19:47:37 +0000

    FInished Assignment

--- lists.py
+++ lists.py
@@ -1,17 +1,29 @@
 def sum_of_odd_nums(n):
-    pass
-
+    l = list(range(1,n*2))
+    new_l = [i for i in l if i%2==1]
+    return sum(new_l)
 def caesar_cipher(message, key):
-    pass
-
+    ordMessage = [ord(i) + key for i in message]
+    bytesMessage = bytes(ordMessage)
+    return bytesMessage.decode("ascii")
+def fizzbuzzHelp(n):
+  if n%3 == 0 and n%5 == 0:
+    return "Fizzbuzz!"
+  elif n%3 == 0:
+    return "Fizz!"
+  elif n%5 == 0:
+    return "Buzz!"
+  else:
+    return n
 def fizzbuzz(n):
-    pass 
+    fizzList = [fizzbuzzHelp(i) for i in range(1,n+1)]
+    return fizzList
 
 def main():
     print('Table of the sum for the first n odd numbers:')
     print('n\tsum')
     print('-'*16)
-    for n in range(10):
+    for n in range(1,11):
         print('{}\t{}'.format(n, sum_of_odd_nums(n)))
 
     print()
